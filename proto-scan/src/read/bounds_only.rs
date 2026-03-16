4592caa253ad83f9c856eecc2b29f47b9a43c59a
Alex Bakon <alexkonradi@gmail.com>
2026-03-16 11:22:34 -0400

    Use [u8; 0] for bounds-only read types

--- proto-scan/src/read/bounds_only.rs
+++ proto-scan/src/read/bounds_only.rs
@@ -2,15 +2,7 @@ use core::convert::Infallible as Never;
 
 use crate::read::{ReadBuffer, ReadError, ReadTypes};
 
-pub struct BoundsOnlyReadTypes(Never);
-
-impl ReadError for BoundsOnlyReadTypes {
-    type Error = Never;
-}
-
-impl ReadTypes for BoundsOnlyReadTypes {
-    type Buffer = [u8; 0];
-}
+pub type BoundsOnlyReadTypes = [u8; 0];
 
 impl ReadBuffer for [u8; 0] {
     type String = &'static str;
@@ -18,4 +10,12 @@ impl ReadBuffer for [u8; 0] {
     fn into_string(self) -> Result<Self::String, core::str::Utf8Error> {
         Ok("")
     }
+}
+
+impl ReadError for [u8; 0] {
+    type Error = Never;
+}
+
+impl ReadTypes for [u8; 0] {
+    type Buffer = [u8; 0];
 }
