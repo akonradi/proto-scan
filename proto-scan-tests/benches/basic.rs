9e92f437e6f2329ecdff54bca1ec620a41a04446
Alex Bakon <alexkonradi@gmail.com>
2026-04-01 06:24:11 -0400

    Add benchmark for extracting from embedded message

--- proto-scan-tests/benches/basic.rs
+++ proto-scan-tests/benches/basic.rs
@@ -4,7 +4,7 @@ use std::sync::LazyLock;
 use gungraun::{library_benchmark, library_benchmark_group, main};
 
 use prost::Message as _;
-use proto_scan::scan::field::Save;
+use proto_scan::scan::field::{Save, ScanOptionalMessage as _};
 use proto_scan::scan::{ScanMessage as _, ScannerBuilder};
 use proto_scan_tests::{prost_proto, proto};
 
@@ -32,6 +32,40 @@ fn byte_types(encoded: &[u8]) -> usize {
     output.bytes_field.len() + output.string_field.len()
 }
 
-library_benchmark_group!(name = bench_basic, benchmarks = byte_types);
+fn embedded_message_input() -> &'static [u8] {
+    static BYTES: LazyLock<Vec<u8>> = LazyLock::new(|| {
+        prost_proto::ScanExample {
+            repeated_bool: vec![true, false, false],
+            single_msg: Some(prost_proto::MultiFieldMessage {
+                flag: true,
+                id: 32918495,
+                name: "name for a message".into(),
+            }),
+            ..Default::default()
+        }
+        .encode_to_vec()
+    });
+
+    &BYTES
+}
+
+#[library_benchmark]
+#[bench::embedded_message(embedded_message_input())]
+fn embedded_message(encoded: &[u8]) -> usize {
+    let scanner = proto::ScanExample::scanner().single_msg(
+        proto::MultiFieldMessage::scanner()
+            .flag(Save)
+            .name(Save)
+            .empty_if_not_present(),
+    );
+
+    let output = scanner.scan(black_box(encoded)).read_all().unwrap();
+    output.single_msg.name.len()
+}
+
+library_benchmark_group!(
+    name = bench_basic,
+    benchmarks = [byte_types, embedded_message]
+);
 
 main!(library_benchmark_groups = bench_basic);
