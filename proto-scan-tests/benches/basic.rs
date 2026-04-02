feaf8e88e768d3b5bc515cabc7c0692da8f288d6
Alex Bakon <alexkonradi@gmail.com>
2026-04-01 21:02:58 -0400

    Add benchmark for parse event reader

--- proto-scan-tests/benches/basic.rs
+++ proto-scan-tests/benches/basic.rs
@@ -1,11 +1,12 @@
 use std::hint::black_box;
 use std::sync::LazyLock;
 
-use gungraun::{library_benchmark, library_benchmark_group, main};
+use gungraun::{LibraryBenchmarkConfig, library_benchmark, library_benchmark_group, main};
 
 use prost::Message as _;
 use proto_scan::scan::field::{Save, ScanOptionalMessage as _};
 use proto_scan::scan::{ScanMessage as _, ScannerBuilder};
+use proto_scan::wire::{LengthDelimited as _, ParseEvent, ParseEventReader as _};
 use proto_scan_tests::{prost_proto, proto};
 
 fn byte_types_inputs() -> &'static [u8] {
@@ -63,9 +64,41 @@ fn embedded_message(encoded: &[u8]) -> usize {
     output.single_msg.name.len()
 }
 
+static LENGTH_DELIMITED_STACK_INPUT: &[u8] = &[
+    0x12, 0x7, 0x1a, 0x5, 0xd, 0x1, 0x2, 0x3, 0x4, 0xd, 0x1, 0x2, 0x3, 0x4,
+];
+
+#[library_benchmark(
+    config = LibraryBenchmarkConfig::default().valgrind_args(["--dump-instr=yes"])
+)]
+#[bench::length_delimited_stack(LENGTH_DELIMITED_STACK_INPUT)]
+fn length_delimited_stack(input: &[u8]) {
+    let mut reader = proto_scan::wire::parse(input);
+
+    let mut read_mid = match reader.next().unwrap().unwrap() {
+        (f, ParseEvent::LengthDelimited(l)) => {
+            assert_eq!(f, 2);
+            l
+        }
+        _ => panic!("wrong event"),
+    }
+    .into_events();
+
+    let read_inner = match read_mid.next().unwrap().unwrap() {
+        (f, ParseEvent::LengthDelimited(l)) => {
+            assert_eq!(f, 3);
+            l
+        }
+        _ => panic!("wrong event"),
+    };
+
+    drop(read_inner);
+    drop(read_mid);
+}
+
 library_benchmark_group!(
     name = bench_basic,
-    benchmarks = [byte_types, embedded_message]
+    benchmarks = [byte_types, embedded_message, length_delimited_stack]
 );
 
 main!(library_benchmark_groups = bench_basic);
